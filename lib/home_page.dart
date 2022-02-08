a78263d2f01cfb9b47efba8eec11fcad8db5290e
Varun Kashyap <20821711+Varun9729@users.noreply.github.com>
2022-02-07 16:20:01 -0800

    Designed top card

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/top_card.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,26 +13,29 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          body: Column(
-        children: [
-          Container(
-            height: 60,
-            child: Center(
-              child: Text('TOTAL BALANCE CARD'),
+          backgroundColor: Colors.grey[200],
+          body: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              children: [
+                TopCard(
+                  balance: '10,000',
+                  income: '100',
+                  expense: '200',
+                ),
+                Expanded(
+                  child: Container(
+                    // color: Colors.red,
+                    child: Center(child: Text('TRANSACTIONS')),
+                  ),
+                ),
+                Container(
+                  height: 30,
+                  child: Center(child: Text("BUTTON")),
+                ),
+              ],
             ),
-          ),
-          Expanded(
-            child: Container(
-              color: Colors.red,
-              child: Center(child: Text('TRANSACTIONS')),
-            ),
-          ),
-          Container(
-            height: 30,
-            child: Center(child: Text("BUTTON")),
-          ),
-        ],
-      )),
+          )),
     );
   }
 }
