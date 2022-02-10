692af9d417939989ccfd00ebebb1f71894780fc6
Varun Kashyap <20821711+Varun9729@users.noreply.github.com>
2022-02-09 16:16:23 -0800

    Added loading

---
+++ lib/loading.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Loading extends StatelessWidget {
+  const Loading({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        height: 25,
+        width: 25,
+        child: CircularProgressIndicator(
+          color: Color(0xff90caf9),
+        ),
+      ),
+    );
+  }
+}
