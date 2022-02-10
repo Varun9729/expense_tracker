a9ee41b2d471f34a897dc519655d382abf233c19
Varun Kashyap <20821711+Varun9729@users.noreply.github.com>
2022-02-09 16:15:21 -0800

    Added Styling to button

--- lib/button.dart
+++ lib/button.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/material.dart';
 
 class PlusButton extends StatelessWidget {
-  const PlusButton({Key? key}) : super(key: key);
+  final function;
+  const PlusButton({Key? key, this.function}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 75,
-      width: 75,
-      decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          color: Color(0xffe3f2fd),
-          border: Border.all(color: Color(0xffbbdefb), width: 1.5)),
-      child: Center(
-          child: Text(
-        "+",
-        style: TextStyle(color: Colors.grey[800], fontSize: 30),
-      )),
+    return GestureDetector(
+      onTap: function,
+      child: Container(
+        height: 75,
+        width: 75,
+        decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Color(0xffe3f2fd),
+            border: Border.all(color: Color(0xffbbdefb), width: 1.5)),
+        child: Center(
+            child: Text(
+          "+",
+          style: TextStyle(color: Colors.grey[800], fontSize: 30),
+        )),
+      ),
     );
   }
 }
